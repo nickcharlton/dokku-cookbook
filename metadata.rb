8938e44542948fa23bc93ecd58138d60ad22e89c
Nick Charlton <nick@nickcharlton.net>
2016-02-19 17:17:09 +0000

    1.0.0 Release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "nick@nickcharlton.net"
 license "mit"
 description "Manages a dokku install."
 long_description "Manages a dokku install and provides LWRPs for managing apps."
-version "0.1.0"
+version "1.0.0"
 
 depends "apt", "~> 2.9"
 depends "docker", "~> 2.0"
