3ba547f1d9e9d603eeceb3766406449cb47ad807
Nick Charlton <nick@nickcharlton.net>
2016-05-14 22:39:41 +0100

    Switch test kitchen to use a real key.
    This isn't used anywhere so it's useless.

--- test/integration/default/serverspec/ssh_keys_spec.rb
+++ test/integration/default/serverspec/ssh_keys_spec.rb
@@ -4,6 +4,7 @@ describe "dokku::ssh_keys" do
   it "puts the ssh key into authorized_keys" do
     authorized_keys = file("/home/dokku/.ssh/authorized_keys")
 
-    expect(authorized_keys).to contain("public_key")
+    expect(authorized_keys).to contain("test")
+    expect(authorized_keys).to contain("AAAAB3NzaC1yc2EAAAADAQABAAACAQDZw0")
   end
 end
